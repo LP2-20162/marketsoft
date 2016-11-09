44757465a8d1a745453549c9795035ae16b12a51
Eliasccotoh <eliascch.200@gmail.com>
2016-11-09 15:15:04 -0500

    distribuidor

--- market_serve/market_service_apps/catalogo/admin.py
+++ market_serve/market_service_apps/catalogo/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 from .models.cliente import Cliente
 from .models.producto import Producto
+from .models.distribuidor import Distribuidor
 
 # Register your models here.
 
@@ -18,4 +19,11 @@ class ProductoAdmin(admin.ModelAdmin):
     list_display = ('lote', 'presentancion', 'TIPO', 'nombre', 'descripcion', 'fecha_expiracion',
                     'fecha_produccion',  'precio_Compra', 'precio_venta', 'stock')
     search_fields = ('nombre', 'descripcion')
+    list_per_page = 3
+
+
+@admin.register(Distribuidor)
+class DistribuidorAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'ruc')
+    search_fields = ('codigo', 'nombre', 'direccion')
     list_per_page = 3
--- market_serve/market_service_apps/catalogo_api/urls.py
+++ market_serve/market_service_apps/catalogo_api/urls.py
@@ -4,12 +4,14 @@ from rest_framework import routers
 
 from .producto_view import ProductoViewSet
 from .cliente_view import ClienteViewSet
+from .distribuidor_view import DistribuidorViewSet
 
 router = routers.DefaultRouter()
 
 
 router.register(r'cliente', ClienteViewSet, 'cliente-view')
 router.register(r'producto', ProductoViewSet, 'producto-view')
+router.register(r'distribuidor', DistribuidorViewSet, 'distribuidor-view')
 
 urlpatterns = [
 
